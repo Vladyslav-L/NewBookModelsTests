f96538f32c13035936e046cdec2d023705709888
Vlad Liashenko <pactur@ukr.net>
2021-04-30 14:45:46 +0300

    test

--- NewBookModelsTests/NewBookModelsTests.cs
+++ NewBookModelsTests/NewBookModelsTests.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using WebDriverManager.Helpers;
+using System.Globalization;
 
 namespace NewBookModelsTests
 {
@@ -14,7 +15,7 @@ namespace NewBookModelsTests
     public class RegistrationTests
     {
         private IWebDriver _webDriver;
-        private IWebDriver _webDriver1;
+        private IWebDriver _webDriver1;        
 
         [SetUp]
         public void Setup()
@@ -46,7 +47,10 @@ namespace NewBookModelsTests
             var searchField3 = _webDriver.FindElement(By.CssSelector(
                "[name=email]"));
 
-            searchField3.SendKeys("sagesar204@sejkt.com");
+            var currentDate = DateTime.Now;
+            string stringDate = currentDate.ToString("yyyyMMddHHmm");
+
+            searchField3.SendKeys($"{stringDate}@gmail.com");
 
             var searchField4 = _webDriver.FindElement(By.CssSelector(
                "[name=password]"));
@@ -65,18 +69,48 @@ namespace NewBookModelsTests
 
             _webDriver.FindElement(By.CssSelector(
              "[class^=SignupForm__submitButton]")).Click();
+                      
+            Thread.Sleep(1000);
+
+            var searchField7 = _webDriver.FindElement(By.CssSelector(
+             "[name=company_name]"));
+
+            searchField5.SendKeys(stringDate);
+
+             var searchField8 = _webDriver.FindElement(By.CssSelector(
+             "[name=company_website]"));
+
+            searchField8.SendKeys("https://newbookmodels.com/");
+
+             _webDriver.FindElement(By.CssSelector(
+             "[name=industry]")).Click();
+
+            _webDriver.FindElement(By.CssSelector(
+             "[class^=Select__optionText]")).Click();
+
+            var searchField9 = _webDriver.FindElement(By.CssSelector(
+            "[name=location]"));
+
+            searchField9.SendKeys("2459 Bentley Ave. Los Angeles CA 90025");
+            
 
 
-            Thread.Sleep(5000);
+
+            searchField9.SendKeys("https://newbookmodels.com/");
+
+
+
+
 
             var actualResult = _webDriver.Url;     
             
-            Assert.AreEqual("https://newbookmodels.com/join/company", actualResult);
+            Assert.AreEqual("https://newbookmodels.com/auth/signin?goBackUrl=%2Fjoin%2Fcompany", actualResult);
         }
 
         [Test]
         public void Test2()
         {
+            
             _webDriver.Navigate().GoToUrl("https://newbookmodels.com/join");
 
             var searchField1 = _webDriver.FindElement(By.CssSelector(
@@ -92,7 +126,10 @@ namespace NewBookModelsTests
             var searchField3 = _webDriver.FindElement(By.CssSelector(
                "[name=email]"));
 
-            searchField3.SendKeys("mhx54105@cuoly.com");
+            var currentDate = new DateTime();
+            string stringDate = currentDate.ToString("yyyyMMddHHmm");
+
+            searchField3.SendKeys($"{currentDate}@gmail.com");
 
             var searchField4 = _webDriver.FindElement(By.CssSelector(
                "[name=password]"));
@@ -145,7 +182,7 @@ namespace NewBookModelsTests
             var searchField1 = _webDriver.FindElement(By.CssSelector(
                "[name=email]"));
 
-            searchField1.SendKeys("mhx54105@cuoly.com");
+            searchField1.SendKeys("currentDate@gmail.com");
 
             var searchField2 = _webDriver.FindElement(By.CssSelector(
                "[name=password]"));
